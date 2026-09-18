6235f6b059a52e852cbb04461c61640c0a35128e
Ian Thuku <thuku0249@gmail.com>
2026-09-18 20:03:27 +0300

    Updates

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import AppNavigator from './src/navigation';
+import AppNavigator from './apps/mobile/src/navigation';
 
 export default function App() {
   return (
